a95275ca418cf0f4ded15c2a17c42a0df7285aad
leonardo.goncalves <leonardo.goncalves@ed.company>
2019-10-08 11:23:29 -0300

    resolvendo error de cors

--- src/Projeto.API/app.js
+++ src/Projeto.API/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('../Projeto.IoC/InjecoesDependencia/ControllerInjecaoDependencia');
+const cors = require('cors');
 const app = express();
 
+const whitelist = ['http://localhost:4201', 'http://localhost:4200', 'http://10.252.1.68:4201', 'http://10.252.1.68:4200']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 app.use(bodyParser.json({ limit: '5mb' }));
+
+
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// })
 
 app.use('', controller.health);
 app.use('/auth', controller.auth);
